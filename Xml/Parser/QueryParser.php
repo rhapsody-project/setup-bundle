bcb92f4fdf7b19c06ca6c012aa5292b4af9d0917
Sean Quinn <swquinn@gmail.com>
2013-12-09 23:47:19 -0500

    General updates (comments, and some functionality).

--- Xml/Parser/QueryParser.php
+++ Xml/Parser/QueryParser.php
@@ -1,4 +1,30 @@
 <?php
+/* Copyright (c) 2013 Rhapsody Project
+ *
+ * Licensed under the MIT License (http://opensource.org/licenses/MIT)
+ *
+ * Permission is hereby granted, free of charge, to any
+ * person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+ * OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 namespace Rhapsody\SetupBundle\Xml\Parser;
 
 use Rhapsody\SetupBundle\Model\Query;
@@ -12,7 +38,7 @@ use Rhapsody\SetupBundle\Xml\XmlElement;
  * @author    Sean W. Quinn <sean.quinn@extesla.com>
  * @category  Rhapsody SetupBundle
  * @package   Rhapsody\SetupBundle\Xml\Parser
- * @copyright Copyright (c) 2012 Rhapsody Project
+ * @copyright Copyright (c) 2013 Rhapsody Project
  * @version   $Id$
  * @since     1.0
  */
